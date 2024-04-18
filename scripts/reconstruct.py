2c2c9a1dcdb873c6ce2b1b11e3681d6cb4f40df9
Monica <a01707439@itesm.mx>
2024-04-18 23:30:24 +0200

    Preprocessing update

--- scripts/reconstruct.py
+++ scripts/reconstruct.py
@@ -1,13 +1,31 @@
 import librosa
 import soundfile as sf
+import numpy as np
+
+def normalize_spectrogram(spectrogram):
+    min_val = np.min(spectrogram)
+    max_val = np.max(spectrogram)
+    normalized = (spectrogram - min_val) / (max_val - min_val)
+    return normalized
+
+def reshape(spectogram):
+    if spectogram.shape >= (1024, 2048):
+        reshaped = spectogram[:1024, :2048, np.newaxis] 
+    else:
+        reshaped = np.zeros((1024, 2048, 1))
+        reshaped[:spectogram.shape[0], :spectogram.shape[1], 0] = spectogram
+
+    return reshaped
 
 def audio_to_stft_to_audio(audio_path, output_path):
     y, sr = librosa.load(audio_path, sr=None)
     stft = librosa.stft(y)
+    stft = normalize_spectrogram(stft)
+    stft = reshape(stft)
     magnitude, phase = librosa.magphase(stft)
     stft_reconstructed = magnitude * phase
     y_reconstructed = librosa.istft(stft_reconstructed)
     sf.write(output_path, y_reconstructed, sr)
 
-audio_to_stft_to_audio('D:\\Semester 8\\AudioGeneration\\test\\188.mp3', 'new.wav')
+audio_to_stft_to_audio('D:\\Semester 8\\AudioGeneration\\dataset\\188.mp3', 'new.wav')
 
--- scripts/stft_preprocessing.py
+++ scripts/stft_preprocessing.py
@@ -33,6 +33,21 @@ def segment_and_save_stft(file_path, output_dir, file_name, segment_length=30, n
         
     return spectrograms
 
+def normalize_spectrogram(spectrogram):
+    min_val = np.min(spectrogram)
+    max_val = np.max(spectrogram)
+    normalized = (spectrogram - min_val) / (max_val - min_val)
+    return normalized
+
+def reshape(spectogram):
+    if spectogram.shape >= (1024, 2048):
+        reshaped = spectogram[:1024, :2048, np.newaxis] 
+    else:
+        reshaped = np.zeros((1024, 2048, 1))
+        reshaped[:spectogram.shape[0], :spectogram.shape[1], 0] = spectogram
+
+    return reshaped
+
 def process_all_files(directory, output_directory):
     all_spectrograms = {}
     i = 0
@@ -42,18 +57,15 @@ def process_all_files(directory, output_directory):
             file_name = os.path.splitext(filename)[0]
             spectrograms = segment_and_save_stft(file_path, output_directory, file_name)
             for spectrogram in spectrograms:
-                all_spectrograms[f'{i}'] = spectrogram  
-                i += 1
+                normalized_spectrogram = normalize_spectrogram(spectrogram)
+                if not np.isnan(normalized_spectrogram).any():
+                    reshaped_spectrogram = reshape(normalized_spectrogram)
+                    all_spectrograms[f'{i}'] = reshaped_spectrogram  
+                    i += 1
 
     return all_spectrograms
-
-def normalize_spectrogram(spectrogram):
-    min_val = np.min(spectrogram)
-    max_val = np.max(spectrogram)
-    return (spectrogram - min_val) / (max_val - min_val)
 
 output_directory = 'stft_images'
 directory = 'D:\\Semester 8\\AudioGeneration\\dataset'
 all_spectrograms = process_all_files(directory, output_directory)
-spectrograms_normalized = {key: normalize_spectrogram(spect) for key, spect in all_spectrograms.items()}
-np.savez_compressed('normalized_stft_spectrograms.npz', **spectrograms_normalized)
+np.savez_compressed('stft_spectrograms.npz', **all_spectrograms)
